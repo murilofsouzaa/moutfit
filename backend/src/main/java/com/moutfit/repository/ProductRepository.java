76645458f7f5312d26fd8e79bf2220f9f7443be4
murilofsouzaa <onemurilo@gmail.com>
2025-11-09 16:55:46 -0300

    feat: CRUD and search methods in ProductService + methods in ProductRepository

--- backend/src/main/java/com/moutfit/repository/ProductRepository.java
+++ backend/src/main/java/com/moutfit/repository/ProductRepository.java
@@ -4,9 +4,13 @@ import com.moutfit.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    Optional<Product> findByName(String name);
+    Optional<Product> findById(long id);
+    List<Product> findAllProductsByName(String name);
+    Optional<Product> findProductByName(String name);
+
 }
--- backend/src/main/java/com/moutfit/services/ProductService.java
+++ backend/src/main/java/com/moutfit/services/ProductService.java
@@ -1,7 +1,60 @@
 package com.moutfit.services;
 
+import com.moutfit.models.Product;
+import com.moutfit.models.User;
+import com.moutfit.repository.ProductRepository;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ProductService {
+
+    ProductRepository productRepository;
+    Product product;
+    User user;
+    List<Product> listProducts;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    public Product findByProductByName(String name){
+        return productRepository.findProductByName(name).orElse(null);
+    }
+
+    public Product addProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    public void removeProduct(Long productId) {
+        productRepository.delete(product);
+    }
+
+    public Product updateName(Long productID, String name) {
+        productRepository.findById(productID);
+        product.setName(name);
+
+        return productRepository.save(product);
+    }
+
+    public Product updatePrice(Long productID, BigDecimal newPrice) {
+        if(!"admin".equals(user.getRole())){
+            throw new SecurityException("You are not allowed to change the price of this product.");
+        }
+
+        Product product = productRepository.findById(productID).orElseThrow(null);
+        product.setPrice(newPrice);
+        return productRepository.save(product);
+    }
+
+
+
+
 }
