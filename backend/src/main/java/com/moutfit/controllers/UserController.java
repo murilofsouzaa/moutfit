c228e2c9880a05769734f2b6a5322a38935ac78c
murilofsouzaa <onemurilo@gmail.com>
2025-11-09 16:24:23 -0300

    structure: User Repository and Product Repository

---
+++ backend/src/main/java/com/moutfit/controllers/UserController.java
@@ -0,0 +1,11 @@
+package com.moutfit.controllers;
+
+import com.moutfit.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+
+}
---
+++ backend/src/main/java/com/moutfit/services/UserService.java
@@ -0,0 +1,14 @@
+package com.moutfit.services;
+
+import com.moutfit.models.User;
+import com.moutfit.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService {
+
+}
